71416045bd35ace77bf4f3a06a9e8a389613a3b7
hyeonwoody <hyeonwoody@gmail.com>
2025-02-22 05:42:09 +0900

    test

--- cmd/api/infrastructure/cache/localCache.go
+++ cmd/api/infrastructure/cache/localCache.go
@@ -31,3 +31,7 @@ func (c *CacheLocal) Get(key string) (model.WorkCache, bool) {
 func (c *CacheLocal) Set(key string, value *hubModel.Work, ttl time.Duration) {
 	c.data[key] = *value.ToCache()
 }
+
+func (c *CacheLocal) a() {
+	//test
+}
